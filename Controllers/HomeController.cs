54ad307cf3e6f3d1a8e6d9192e7e79b2e0edf421
Nick D'Oria <17dorin@gmail.com>
2021-03-02 17:11:12 -0500

    Finished lab

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SWAPILab.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SWAPILab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private SwapiDAL swd = new SwapiDAL();
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult People(int id)
+        {
+            Person p = swd.ConvertToPerson(id);
+            return View(p);
+        }
+
+        public IActionResult Planets(int id)
+        {
+            Planet p = swd.ConvertToPlanet(id);
+            return View(p);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/SwapiDAL.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace SWAPILab.Models
+{
+    public class SwapiDAL
+    {
+        public string GetData(int id, string section)
+        {
+            string url = $@"https://swapi.dev/api/{section}/{id}/";
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string json = rd.ReadToEnd();
+
+            return json;
+        }
+
+        public Person ConvertToPerson(int id)
+        {
+            string personData = GetData(id, "people");
+            Person p = JsonConvert.DeserializeObject<Person>(personData);
+
+            return p;
+        }
+
+        public Planet ConvertToPlanet(int id)
+        {
+            string planetData = GetData(id, "planets");
+            Planet p = JsonConvert.DeserializeObject<Planet>(planetData);
+
+            return p;
+        }
+    }
+}
